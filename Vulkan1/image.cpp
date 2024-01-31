7f79507276983ab60e0f71bada6f07ea35d0adc9
pachiko <phillippang1994@gmail.com>
2024-01-31 11:33:22 +0800

    Fix wrong number of mip levels

--- Vulkan1/image.cpp
+++ Vulkan1/image.cpp
@@ -28,7 +28,7 @@ VkImageView Application::createImageView(VkImage image, VkFormat format, VkImage
 void Application::createImageViews() {
     swapChainImageViews.resize(swapChainImages.size());
     for (size_t i = 0; i < swapChainImages.size(); i++) {
-        swapChainImageViews[i] = createImageView(swapChainImages[i], swapChainImageFormat, VK_IMAGE_ASPECT_COLOR_BIT, mipLevels);
+        swapChainImageViews[i] = createImageView(swapChainImages[i], swapChainImageFormat, VK_IMAGE_ASPECT_COLOR_BIT, 1);
     }
 }
 
