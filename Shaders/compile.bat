627e16cf9a719c60b0a4dd3f2a2189c8de6ccb9a
pachiko <phillippang1994@gmail.com>
2024-05-26 15:02:08 +0800

    rename shader binaries

--- Shaders/compile.bat
+++ Shaders/compile.bat
@@ -1,3 +1,3 @@
-C:/VulkanSDK/1.3.268.0/Bin/glslc.exe shader.vert -o vert.spv
-C:/VulkanSDK/1.3.268.0/Bin/glslc.exe shader.frag -o frag.spv
+C:/VulkanSDK/1.3.268.0/Bin/glslc.exe shader.vert -o shader.vert.spv
+C:/VulkanSDK/1.3.268.0/Bin/glslc.exe shader.frag -o shader.frag.spv
 pause
